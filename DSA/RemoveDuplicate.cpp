9758263cb9128f2519b3d85eb297bfe99c66cd9c
j-kashish <kj67662@gmail.com>
2025-02-06 22:58:14 +0530

    06/02/25

---
+++ DSA/RemoveDuplicate.cpp
@@ -0,0 +1,10 @@
+int removeDups(int arr[] ,int n){
+    int res=1;
+    for(int i=1;i<n;i++){
+        if (arr[i]!= arr[res-1]){
+            arr[res]=arr[i];
+            res++;
+        }
+    }
+    return res;
+}
---
+++ DSA/checkArrayIsSorted.cpp
@@ -0,0 +1,7 @@
+bool isSorted(int arr[],int n){
+    for(int i=1;i<n;i++)
+    if (arr[i] < arr[i-1]){
+        return false;
+    }
+    return true;
+}
